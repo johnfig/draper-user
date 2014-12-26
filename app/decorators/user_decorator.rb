fa9c0384122f1e946b933038399415e3e793db48
John Figueiredo <john@johnfigueiredo.com>
2014-12-26 12:57:58 -0800

    Change #salary_string to use ActionView NumberHelper

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,6 @@
 class UserDecorator < Draper::Decorator
+  include ActionView::Helpers::NumberHelper
+
   def full_name
     [object.first_name, object.last_name].join(' ')
   end
@@ -8,21 +10,6 @@ class UserDecorator < Draper::Decorator
   end
 
   def salary_string
-    array = object.salary.to_s.split('').reverse
-    new_array = []
-    array.each_with_index do |letter, index|
-      divider = index + 1
-      if divider % 3 == 0
-        if array.last == letter
-          new_array << letter
-        else
-          new_array << ",#{letter}"
-        end
-      else
-        new_array << letter
-      end
-    end
-    new_array.push('$')
-    new_array.reverse.join
+    number_to_currency(object.salary)
   end
 end

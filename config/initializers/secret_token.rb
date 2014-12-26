8a8095ac6ab1eb136fb5fa089d7dc88d459b4090
John Figueiredo <john@johnfigueiredo.com>
2014-12-26 10:11:37 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DraperUser::Application.config.secret_key_base = '871e99f8992024862b75cbe48e8c9bcca1d4119ad5cfa7855df014135865acbd1d2ad6e508e57f7a8527895b7b52c2dacafc83a601e2d7b4d025cb1daa0ee10f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DraperUser::Application.config.session_store :cookie_store, key: '_draper-user_session'

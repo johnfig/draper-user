8587a37c8bbab6e30f0e077df81355d991b0c4ba
John Figueiredo <john@johnfigueiredo.com>
2014-12-26 10:55:49 -0800

    Add formatted salary string to UserDecorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -6,4 +6,23 @@ class UserDecorator < Draper::Decorator
   def formatted_birthday
     object.birthday.strftime('%F')
   end
+
+  def salary_string
+    array = object.salary.to_s.split('').reverse
+    new_array = []
+    array.each_with_index do |letter, index|
+      divider = index + 1
+      if divider % 3 == 0
+        if array.last == letter
+          new_array << letter
+        else
+          new_array << ",#{letter}"
+        end
+      else
+        new_array << letter
+      end
+    end
+    new_array.push('$')
+    new_array.reverse.join
+  end
 end
